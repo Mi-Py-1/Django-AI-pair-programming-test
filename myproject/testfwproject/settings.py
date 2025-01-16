9ef73d7bbee544c31159ce8f72cf25806134d2ba
Michael Pymer <mikepymer@hotmail.co.uk>
2025-01-16 13:59:26 +0000

    added: connectivity and allowances in other files requirements, settings, urls

--- myproject/testfwproject/settings.py
+++ myproject/testfwproject/settings.py
@@ -123,3 +123,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
